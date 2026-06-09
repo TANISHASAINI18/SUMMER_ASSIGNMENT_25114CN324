86575538444f5ce7db4f37bf11eb4734fc6aafb1
TANISHASAINI18 <jiyasaini18092007@gmail.com>
2026-06-09 20:13:02 +0530

    Day-9 is uploaded

---
+++ Day-9/code35.c
@@ -0,0 +1,15 @@
+//code35
+#include<stdio.h>
+int main()
+{
+    int i, j;
+      for(i = 1; i <= 5; i++)  
+    {
+        char ch = 'A' + i - 1;   
+        for(j = 1; j <= i; j++) {
+            printf("%c", ch);
+        }
+      printf("\n");   
+    }
+     return 0;
+}
