c66921a58c29ef8650f60ae2383eff81dd231a0f
TANISHASAINI18 <jiyasaini18092007@gmail.com>
2026-06-15 00:44:44 +0530

    Day-15 is uploaded

---
+++ Day-15/code57.c
@@ -0,0 +1,16 @@
+//code57
+#include <stdio.h>
+int main() {
+    int arr[100], n, i;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter elements:\n");
+    for(i=0;i<n;i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Reversed array is:\n");
+    for(i=n-1;i>=0;i--) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
